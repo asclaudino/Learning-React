98d60f8bc0e1e3590bf8b1b3368559928da819ee
asclaudino <arielsclaudino@gmail.com>
2022-12-20 22:03:10 -0300

    cleaning comments

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,19 +12,6 @@ const Expenses = (props) => {
     setSelectedYear(choosenYear);
   };
 
-  // const expensesListComponents = () => {
-  //   const expensesFiltered = props.items.filter(
-  //     (expense) => toString(expense.date.getFullYear()) === selectedYear
-  //   );
-  //   return expensesFiltered.map((expense) => (
-  //     <ExpenseItem
-  //       key={expense.id}
-  //       title={expense.title}
-  //       amount={expense.amount}
-  //       date={expense.date}
-  //     ></ExpenseItem>
-  //   ));
-  // };
 
   return (
     <div>
@@ -34,7 +21,6 @@ const Expenses = (props) => {
           selected={selectedYear}
           className="expenses-filter"
         ></ExpensesFilter>
-        {/* {expensesListComponents()} */}
         {props.items
           .filter(
             (expense) => expense.date.getFullYear() === Number(selectedYear)
@@ -47,26 +33,6 @@ const Expenses = (props) => {
               date={expense.date}
             ></ExpenseItem>
           ))}
-        {/* <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        ></ExpenseItem> */}
       </Card>
     </div>
   );
